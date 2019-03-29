fc86d961a19662250843b398d0cc8423a4739606
Thomas G <thomas_grefsrud@hotmail.com>
2019-03-29 10:35:39 +0100

    's'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
+/* eslint-disable */
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    >
+    <h1>Robot jail</h1>
+    <img alt="Vue logo" src="./assets/jail.png">
+    <!--<HelloWorld msg="FAKEJson fun times"/>-->
+    <div class="row">
+      <div class="col" v-for="criminal in criminals" v-bind:key="criminal.id">
+        <convictCard
+          :id="criminal.id"
+          :name="criminal.givenName + ' ' + criminal.familyName"
+          :crime="criminal.crime"
+          :headshot="criminal.headshot"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import convictCard from "./components/convictCard.vue";
+import data from "./assets/data.json";
+
+//const axios = require('axios')
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    convictCard
+  },
+  data() {
+    return {
+      criminals: null
+    };
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      /*
+      let criminals;
+      let payload = {
+        token: "CaKtpLTHh1Fi3Nh-FawIew",
+        data: {
+          id: "numberInt",
+          headshot: "personAvatar",
+          givenName: "nameFirst",
+          familyName: "nameLast",
+          crime: "stringWords|5,8",
+           _repeat: 10
+        }
+      };
+      await axios({
+        method: "post",
+        url: "https://app.fakejson.com/q",
+        data: payload
+      }).then(function(resp) {
+        criminals = resp.data
+      });*/
+      console.log(data);
+      this.criminals = data;
+      //console.log(this.criminals)
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #a8a8a8
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/convictCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Crime">
+    <h3>{{name}}</h3>
+    <img v-bind:src="headshot">
+    <p><strong>Crime: </strong>{{crime}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "convictCard",
+  props: {
+    name: String,
+    id: String,
+    headshot: String,
+    crime: String
+  }
+};
+</script>
+
+<style>
+.Crime {
+    background: #1c083d;
+    border: solid 2px #e94210;
+    border-radius: 50px 50px 10px;
+    margin-top: 1%;
+}
+
+h3, p {
+    color: whitesmoke;
+}
+</style>
